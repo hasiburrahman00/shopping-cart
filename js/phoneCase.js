0f1423aa9a142774a67feed1f2ddcb5fd6154ab0
hasibur_rahman <hasiburuoda@gmail.com>
2023-02-15 17:50:51 +0600

    phone and phone case add section done - allhumdulilah

--- js/phoneCase.js
+++ js/phoneCase.js
@@ -2,30 +2,52 @@ const casePrice = document.getElementById('case-price');
 const casePlus = document.getElementById('case-plus');
 const caseMinus = document.getElementById('case-minus');
 const caseCount = document.getElementById('case-counter');
+const phonePrice = document.getElementById('phone-price');
+const phonePlus = document.getElementById('phone-plus');
+const phoneMinus = document.getElementById('phone-minus');
+const phonecount = document.getElementById('phone-count');
 
-function caseCounter(condition) {
-    const caseCounter = caseCount.value;
-    let updateCaseCount = 0;
-    const caseCountValue = parseInt(caseCounter)
-    if(condition) {
-         updateCaseCount = caseCountValue + 1;
-    }else {
-         updateCaseCount = caseCountValue - 1;
+
+function caseCounterFunc(isIncrease, product) {
+    
+    let updateProductCount;
+
+    if (isIncrease) {
+        updateProductCount = parseInt(product.value) + 1;
+    } else {
+        updateProductCount = parseInt(product.value) - 1;
     }
-    caseCount.value = updateCaseCount;
-    return updateCaseCount;
+    product.value = updateProductCount;
+    return updateProductCount;
 }
 
 
 
-casePlus.addEventListener('click', function() {
-    const caseNumber = caseCounter(true);
-    const caseFinalPrice = caseNumber*59;
+casePlus.addEventListener('click', function () {
+    const caseNumber = caseCounterFunc(true, caseCount);
+    const caseFinalPrice = caseNumber * 59;
     casePrice.innerText = caseFinalPrice;
 })
 
-caseMinus.addEventListener('click', function() {
-    const caseNumber = caseCounter(false);
-    const caseFinalPrice = caseNumber*59;
+caseMinus.addEventListener('click', function () {
+
+    const caseNumber = caseCounterFunc(false, caseCount);
+    const caseFinalPrice = caseNumber * 59;
     casePrice.innerText = caseFinalPrice;
+})
+
+
+
+
+
+phonePlus.addEventListener('click', function () {
+    const phoneNumber = caseCounterFunc(true, phonecount);
+    const phoneFinalPrice = phoneNumber * 1219;
+    phonePrice.innerText = phoneFinalPrice;
+})
+
+phoneMinus.addEventListener('click', function () {
+    const phoneNumber = caseCounterFunc(false, phonecount);
+    const phoneFinalPrice = phoneNumber * 1219;
+    phonePrice.innerText = phoneFinalPrice;
 })
